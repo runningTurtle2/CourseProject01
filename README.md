2d381746a05a5f238b19c843395a7287f52125c5
runningTrutle2 <oscarmoren2010@gmail.com>
2023-05-13 19:46:14 -0700

    Updated ReadME w/ task to complete. Modded js

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # CourseProject01
+
+## TASKS
+
+**Priority** 
+- Use pagebeforeshow event so when the user navigates tot he display page the data is already there
+- 4th page does something else with data **BUT** does not need to work this week
+- Needs to be clear what it does.
+
+**Optional ideas**
+- Maybe use popout to display the trailers
+
+# Version 2
+
+## TASKS
+
+**Priority** 
+- Display page <li> elements must be clickable
+- Each clickable li element must do something unique relative to that particular <li>
--- main.js
+++ main.js
@@ -9,12 +9,12 @@ let Game = function (pTitle, pGenre, pRelease, pURL) {
   this.ID = Math.random().toString(16).slice(5); // Unique ID per object created
 };
 
-gameArr.push(new Game("Red Dead Redemption 2", "Action; Open World", "10/26/2018","https://www.youtube.com/embed/HVRzx17WHVk"))
-gameArr.push(new Game("Pokémon Legends: Arceus", "Adventure; Open World", "1/28/2022", "https://www.youtube.com/embed/I4RynqpahT8"))
-gameArr.push(new Game("Assassin's Creed Mirage", "Action; Rogue-like", "TBA 2023", "https://www.youtube.com/embed/x55lAlFtXmw"))
-gameArr.push(new Game("Diablo 4", "Action; Adventure", "6/6/2023", "https://www.youtube.com/embed/Ro26B394ZBM"))
-gameArr.push(new Game("The Legend of Zelda: Tears of the Kingdom", "Action; Open World", "5/12/2023", "https://www.youtube.com/embed/uHGShqcAHlQ"))
-gameArr.push(new Game("Hollow Knight: Silksong", "Action; Adventure", "TBA 2023","https://www.youtube.com/embed/JSfuFlhsxZY"))
+gameArr.push(new Game("Red Dead Redemption 2","Action; Open World","10/26/2018","https://www.youtube.com/embed/HVRzx17WHVk"));
+gameArr.push(new Game("Pokémon Legends: Arceus","Adventure; Open World","1/28/2022","https://www.youtube.com/embed/I4RynqpahT8"));
+gameArr.push(new Game("Assassin's Creed Mirage","Action; Rogue-like","TBA 2023","https://www.youtube.com/embed/x55lAlFtXmw"));
+gameArr.push(new Game("Diablo 4","Action; Adventure","6/6/2023","https://www.youtube.com/embed/Ro26B394ZBM"));
+gameArr.push(new Game("The Legend of Zelda: Tears of the Kingdom","Action; Open World","5/12/2023","https://www.youtube.com/embed/uHGShqcAHlQ"));
+gameArr.push(new Game("Hollow Knight: Silksong","Action; Adventure","TBA 2023","https://www.youtube.com/embed/JSfuFlhsxZY"));
 
 document.addEventListener("DOMContentLoaded", function () {
   createList();
@@ -40,18 +40,44 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("genre").value = "";
     document.getElementById("release").value = "";
     document.getElementById("URL").value = "";
-console.log(ID)
+
     // refresh library
     createList();
   });
 
-    document.getElementById("buttonClear").addEventListener("click", function () {
+  document.getElementById("buttonClear").addEventListener("click", function () {
     document.getElementById("title").value = "";
     document.getElementById("genre").value = "";
     document.getElementById("release").value = "";
     document.getElementById("URL").value = "";
   });
 });
+
+$(document).on("pagebeforeshow", "#library", function (event) {
+  // have to use jQuery
+  createList();
+});
+
+/************************************************************************************** */
+// need one for our details page to fill in the info based on the passed in ID
+$(document).on("pagebeforeshow", "#library", function (event) {
+  let localID = localStorage.getItem("parm"); // get the unique key back from the dictionairy
+
+
+  
+ gameArr = JSON.parse(localStorage.getItem("gameArr"));
+  //let pointer = GetArrayPointer(localID);     // Crashing display page
+
+/*   document.getElementById("gamelist").innerHTML =
+    "The title is: " + gameArr[pointer].Title;
+  document.getElementById("gamelist").innerHTML =
+    "Year released: " + gameArr[pointer].release;
+  document.getElementById("gamelist").innerHTML =
+    "Genre: " + gameArr[pointer].Genre;
+  document.getElementById("gamelist").innerHTML = gameArr[pointer].URL; */
+});
+
+/****************************************************************************************** */
 
 function createList() {
   let myUL = document.getElementById("gameList");
@@ -77,3 +103,11 @@ function createList() {
     myUL.appendChild(myLi);
   });
 }
+
+function GetArrayPointer(localID) {
+  for (let i = 0; i < gamearr.length; i++) {
+      if (localID === gamearr[i].ID) {
+          return i;
+      }
+    }
+  }
